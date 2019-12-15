a9d528d7b7b6a35736545a74b0c458e1ce765442
synerzip <ganeshkaspate1@gmail.com>
2019-12-15 17:18:10 +0530

    Added one route

--- facebookalbum/src/App.js
+++ facebookalbum/src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Home from './modules/Home/component/Home';
+import Login from './modules/Login/component/Login';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App-container">
+      <Switch>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/'>
+          <Redirect
+            to={{
+              pathname: '/facebook-albums'
+            }}
+          />
+        </Route>
+      </Switch>
+    </div >
   );
 }
 
